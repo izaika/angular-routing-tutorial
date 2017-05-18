b1bb1505a51075178df2e8a2638c35d0d5f2bf19
Igor Zaika <izaika89@gmail.com>
2017-05-18 15:22:15 +0300

    added individual server route

--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -1,19 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {ActivatedRoute, Params} from '@angular/router';
 
-import { ServersService } from '../servers.service';
+import {Subscription} from 'rxjs/Subscription';
+
+import {ServersService} from '../servers.service';
 
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
   styleUrls: ['./server.component.css']
 })
-export class ServerComponent implements OnInit {
-  server: {id: number, name: string, status: string};
+export class ServerComponent implements OnInit, OnDestroy {
+  server: { id: number, name: string, status: string };
+  paramsSubscription: Subscription;
 
-  constructor(private serversService: ServersService) { }
+  constructor(private serversService: ServersService, private route: ActivatedRoute) {
+  }
 
   ngOnInit() {
-    this.server = this.serversService.getServer(1);
+    this.server = this.serversService.getServer(+this.route.snapshot.params['id']);
+
+    this.paramsSubscription = this.route.params.subscribe((params: Params) => {
+      this.server = this.serversService.getServer(+params['id']);
+    });
+  }
+
+  ngOnDestroy() {
+    this.paramsSubscription.unsubscribe();
   }
 
 }
