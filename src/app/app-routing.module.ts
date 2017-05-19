1a0eb828af910b8045d269ceef34b5986631d456
Igor Zaika <izaika89@gmail.com>
2017-05-19 11:33:29 +0300

    fake auth middleware

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,6 +8,7 @@ import {ServersComponent} from './servers/servers.component';
 import {ServerComponent} from './servers/server/server.component';
 import {EditServerComponent} from './servers/edit-server/edit-server.component';
 import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
+import {AuthGuard} from './auth-guard.service';
 
 const appRoutes: Routes = [
   {
@@ -27,6 +28,8 @@ const appRoutes: Routes = [
   {
     path: 'servers',
     component: ServersComponent,
+    // canActivate: [AuthGuard],
+    canActivateChild: [AuthGuard],
     children: [
       {
         path: ':id',
